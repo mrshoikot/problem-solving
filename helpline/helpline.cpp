2fac8685d4e3023188aacf04dbcce85cdc9afd14
mrshoikot <mrshoikot@gmail.com>
2021-07-11 04:46:35 +0600

    Time calculation helpline

--- helpline/helpline.cpp
+++ helpline/helpline.cpp
@@ -1,1 +1,13 @@
 // g++ -o output test.cpp && ./output
+
+
+// Time calculation
+clock_t tStart = clock();
+printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+// Loop through map
+map<long long int,long long int>::iterator itr;
+for (itr = data.begin(); itr != data.end(); ++itr) {
+    cout << '\t' << itr->first
+            << '\t' << itr->second << '\n';
+}
---
+++ helpline/helpline.py
@@ -0,0 +1,8 @@
+# Runtime calculation
+import timeit
+
+
+start = timeit.default_timer()
+# Code HERE
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
